d419b11bd71ea94de5c04d43fd56cbcf34578861
Trincasidra <15145418+trincasidra@users.noreply.github.com>
2021-03-24 20:43:35 +0100

    Revert to enter/leave combat for aggro reasons.

--- oUF_Simple/core/functions.lua
+++ oUF_Simple/core/functions.lua
@@ -103,7 +103,7 @@ end
 
 --UpdateThreat
 local function UpdateThreat(self,event,unit)
-  if event == "PLAYER_REGEN_ENABLED" or event == "PLAYER_REGEN_DISABLED" then
+  if event == "PLAYER_LEAVE_COMBAT" or event == "PLAYER_ENTER_COMBAT" then
     --do natting
   elseif self.unit ~= unit then
     return
@@ -258,8 +258,8 @@ local function CreateHealthBar(self)
   --hooks
   s.PostUpdate = PostUpdateHealth
   if s.colorThreat then
-    self:RegisterEvent("PLAYER_REGEN_ENABLED", L.F.UpdateThreat, true)
-    self:RegisterEvent("PLAYER_REGEN_DISABLED", L.F.UpdateThreat, true)
+    self:RegisterEvent("PLAYER_LEAVE_COMBAT", L.F.UpdateThreat, true)
+    self:RegisterEvent("PLAYER_ENTER_COMBAT", L.F.UpdateThreat, true)
     self:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE", L.F.UpdateThreat, true)
     self:RegisterEvent("UNIT_THREAT_LIST_UPDATE", L.F.UpdateThreat, true)
   end
